c68b5d8ca6e36a7ec651072575b44cf7ee2977d2
Historyk <kerian.georges@iut-rodez.fr>
2017-06-01 22:03:21 +0200

    résolution bug tourprécédent du à un conflit mal régler

--- src/othello/Partie.java
+++ src/othello/Partie.java
@@ -141,15 +141,12 @@ public class Partie implements Serializable {
 	 * dans une partie joueur contre joueur et détermine les coups possibles
 	 */
 	public void tourPrecedent() {
-		doitJouer = (doitJouer + 1) % 2 ;
-		plateauDeJeu.determinerCoupsPossibles(
-				                          listeJoueur[doitJouer].getCouleur());
-		this.actualiserHistoriqueCoups();
+
 		tour--;
 		this.annulationCoupPrecedent();
 		this.actualiserHistoriqueCoups();
 		Plateau.coupsPossibles.clear();
-		doitJouer = (doitJouer + 1) % 2;
+		doitJouer = (doitJouer + 1) % 2 ;
 		plateauDeJeu.determinerCoupsPossibles(
 										listeJoueur[doitJouer].getCouleur());		
 	}
