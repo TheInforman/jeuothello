d3c8fadc71754305e9db23c70544dc5fb9deb93a
Historyk <kerian.georges@iut-rodez.fr>
2017-06-02 00:22:40 +0200

    testPartie finit (partie jouable en console)

--- src/othello/tests/testPartie.java
+++ src/othello/tests/testPartie.java
@@ -29,42 +29,74 @@ public class testPartie {
 	}
 
 	private static void testPartieJouable() {
-		// Test primaire
-		Joueur joueur1 = new Joueur("Joueur1", 0);
-		Joueur joueur2 = new Joueur("Joueur2", 1);
-		Partie partie = new Partie(joueur1, joueur2, 0);
 
-		System.out.println(partie);
-		partie.tourSuivant();
-		System.out.println(partie);
 
-		System.out.println("\n\n\n\n\n\n\n\n");
-
-		// Test déroulement de 2 tour 
+		/* Test d'une partie en console */
 		Joueur player1 = new Joueur("Test",0);
 		Joueur player2 = new Joueur(1);
-		Partie partieTest = new Partie(player1, player2, 0);
+		Partie partieTest = new Partie(player2, player1, 0);
 		Plateau courant = partieTest.getPlateau();
-
-		// Fais jouer un tour au joueur 1
-		courant.determinerCoupsPossibles(0);
+		
+		/* boolean gérant la fin de la partie */
+		boolean partieEnCours = true;
+		
+		/* nombre de pion blanc et noir sous le contrôle des joueurs */
+		int nbBlanc = 0;
+		int nbNoir = 0;
+		
+		/* on détermine les coups possibles pour le premier joueur */
+		courant.determinerCoupsPossibles(partieTest.getDoitJouer());
 		System.out.println(partieTest.getPlateau());
-
-		while(partieTest.getTour() < 60 ) {
+		
+		// boucle de jeu d'une partie console
+		while(partieEnCours) {
+			/* test si les coordonées entrées sont correct, si oui applique 
+			   le coup, sinon continue à demander des coordonées */
 			while( courant.isActionEffectuee() == false ){
 				courant.appliquerCoups(OutilsConsole.demanderCase(courant),
-				partieTest.getListeJoueur()[partieTest.getDoitJouer()]
-						  .getCouleur());
+						partieTest.getListeJoueur()[partieTest.getDoitJouer()]
+								.getCouleur());
 			}
+			/* passe au tour suivant */
 			partieTest.tourSuivant();
+			
+			/* affiche le plateau et les coups possibles */
 			System.out.println(partieTest.getPlateau());
+			
+			/* réinitialise la condition pour poser les pions */
 			courant.setActionEffectuee(false);
+			
+			/* test si le joueur peut jouer (si il existe des coups possibles)
+			 s'il n'en existe pas, on passe le tour */
+			if(partieTest.getPlateau().getCoupsPossibles().isEmpty()) {
+				/* Récupère le pseudo du joueur jouant le tour actuel */
+				String pseudoJoueur =
+						partieTest.getListeJoueur()
+						[partieTest.getDoitJouer()].getNom();
+				/* on passe au tour suivant */
+				partieTest.tourSuivant();
+
+				/* Si deux fois d'affiler on ne peut pas jouer, on finit la partie
+				   sinon on affiche un message comme quoi le joueur précédent
+				    n'a pas pu jouer puis on affiche le plateau et les coups possibles */
+				if(partieTest.getPlateau().getCoupsPossibles().isEmpty()) {
+					partieEnCours = false;
+				} else {
+					System.out.println("Notification de Partie\n"
+							+"Le tour de "+ pseudoJoueur + " à  été passé "
+							+ "car il ne pouvait pas agir.");
+					System.out.println(partieTest.getPlateau());
+
+				}
+
+			}
+
+			/* calcul le score */
+			nbBlanc = courant.calculerNbPions(0);
+			nbNoir = courant.calculerNbPions(1);
+			System.out.println("Score : " + nbBlanc + " à " + nbNoir );
 		}
-
-
-		int nbBlanc = courant.calculerNbPions(0);
-		int nbNoir = courant.calculerNbPions(1);
-		System.out.println("Score : " + nbBlanc + " à " + nbNoir );
+		/* affiche le score final */
+		System.out.println("\n\n\nScore final : " + nbBlanc + " à " + nbNoir );
 	}
-
 }
